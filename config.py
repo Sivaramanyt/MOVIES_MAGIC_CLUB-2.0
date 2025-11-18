ccf0fd6b1654d24d3b13cd9ae134627422f3a11b
Rolex-444 <sivaraman.gc49@gmail.com>
2025-11-18 12:22:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,6 +9,9 @@ VERIFICATION_DEFAULT_ENABLED = True
 VERIFICATION_DEFAULT_FREE_LIMIT = 3
 VERIFICATION_DEFAULT_VALID_MINUTES = 1440  # 24 hours
 
+SHORTLINK_API = "your-shortlink-api-key-here"
+SHORTLINK_URL = "https://your-shortlink-service-api-endpoint-here"
+
 if not API_ID or not API_HASH or not BOT_TOKEN:
     print("❌ Please set API_ID, API_HASH, BOT_TOKEN as env variables")
 
